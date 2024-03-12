e9ae49de44122929b5d895218775b57d61d02863
BintangDiandra <Bintangdiandraputra@gmail.com>
2024-03-12 16:08:12 +0700

    initial commit

---
+++ javascriptnavbar.js
@@ -0,0 +1,53 @@
+    // Burger menus
+    document.addEventListener("DOMContentLoaded", function () {
+    // open
+    const burger = document.querySelectorAll(".navbar-burger");
+    const menu = document.querySelectorAll(".navbar-menu");
+  
+    if (burger.length && menu.length) {
+      for (var i = 0; i < burger.length; i++) {
+        burger[i].addEventListener("click", function () {
+          for (var j = 0; j < menu.length; j++) {
+            menu[j].classList.toggle("hidden");
+          }
+        });
+      }
+    }
+  
+    // close
+    const close = document.querySelectorAll(".navbar-close");
+    const backdrop = document.querySelectorAll(".navbar-backdrop");
+  
+    if (close.length) {
+      for (var i = 0; i < close.length; i++) {
+        close[i].addEventListener("click", function () {
+          for (var j = 0; j < menu.length; j++) {
+            menu[j].classList.toggle("hidden");
+          }
+        });
+      }
+    }
+  
+    if (backdrop.length) {
+      for (var i = 0; i < backdrop.length; i++) {
+        backdrop[i].addEventListener("click", function () {
+          for (var j = 0; j < menu.length; j++) {
+            menu[j].classList.toggle("hidden");
+          }
+        });
+      }
+    }
+  });
+  
+  
+  // Dark Mode & light mode
+  function toggleMode() {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-mode');
+  }
+
+
+  const y = 3
+  const x = 4
+  const z = y * x
+  console.log(`Hasil Y kali x adalah = ${z}`)
